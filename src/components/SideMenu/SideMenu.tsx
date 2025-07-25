a9729e9f922f03ba844b0e90d701a80f27f48413
Evgenii S. <evgenii.shigabutdinov@gcore.com>
2025-07-25 17:55:45 +0400

    Remove Redoc attribution

--- src/components/SideMenu/SideMenu.tsx
+++ src/components/SideMenu/SideMenu.tsx
@@ -8,8 +8,6 @@ import { OptionsContext } from '../OptionsProvider';
 import { MenuItems } from './MenuItems';
 
 import { PerfectScrollbarWrap } from '../../common-elements/perfect-scrollbar';
-import { RedocAttribution } from './styled.elements';
-import RedoclyLogo from './Logo';
 
 @observer
 export class SideMenu extends React.Component<{ menu: MenuStore; className?: string }> {
@@ -32,12 +30,6 @@ export class SideMenu extends React.Component<{ menu: MenuStore; className?: str
         }}
       >
         <MenuItems items={this.filteredItems} onActivate={this.activate} root={true} />
-        <RedocAttribution>
-          <a target="_blank" rel="noopener noreferrer" href="https://redocly.com/redoc/">
-            <RedoclyLogo />
-            API docs by Redocly
-          </a>
-        </RedocAttribution>
       </PerfectScrollbarWrap>
     );
   }
--- src/components/SideMenu/styled.elements.ts
+++ src/components/SideMenu/styled.elements.ts
@@ -1,8 +1,7 @@
 import * as classnames from 'classnames';
-import { darken } from 'polished';
 
 import { deprecatedCss, ShelfIcon } from '../../common-elements';
-import styled, { css, media, ResolvedThemeInterface } from '../../styled-components';
+import styled, { css, ResolvedThemeInterface } from '../../styled-components';
 
 export const OperationBadge = styled.span.attrs((props: { type: string; color?: string }) => ({
   className: `operation-type ${props.type}`,
@@ -175,36 +174,4 @@ export const MenuItemTitle = styled.span<{ width?: string }>`
   width: ${props => (props.width ? props.width : 'auto')};
   overflow: hidden;
   text-overflow: ellipsis;
-`;
-
-export const RedocAttribution = styled.div`
-  ${({ theme }) => css`
-    font-size: 0.8em;
-    margin-top: ${theme.spacing.unit * 2}px;
-    text-align: center;
-    position: fixed;
-    width: ${theme.sidebar.width};
-    bottom: 0;
-    background: ${theme.sidebar.backgroundColor};
-
-    a,
-    a:visited,
-    a:hover {
-      color: ${theme.sidebar.textColor} !important;
-      padding: ${theme.spacing.unit}px 0;
-      border-top: 1px solid ${darken(0.1, theme.sidebar.backgroundColor)};
-      text-decoration: none;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-    }
-  `};
-  img {
-    width: 15px;
-    margin-right: 5px;
-  }
-
-  ${media.lessThan('small')`
-    width: 100%;
-  `};
 `;
